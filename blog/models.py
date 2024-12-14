4d53f10209ce49cf202d8513ad7089278d15cb95
RicardoMor98 Moreira <ricardo.moreira98@gmail.com>
2024-12-14 11:08:23 +0000

    Add image model

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
     class Meta:
         ordering = ["-created_on"]
 
